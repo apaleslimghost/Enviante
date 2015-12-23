52c8b23398b9f9b9452923617113cb72e5676e7c
Matt Brennan <mattyb1000@gmail.com>
2015-12-23 21:09:56 +0000

    imports

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
-var im = require('immutable');
-var Bacon = require('baconjs');
-var mapToTree = require('@quarterto/immutable-map-to-tree');
+import {Map, Iterable, List, fromJS as immutableFromJS} from 'immutable';
+import {fromBinder as baconFromBinder, Bus} from 'baconjs';
+import mapToTree from '@quarterto/immutable-map-to-tree';
 
 export default function dispatcher(receivers, init = {}) {
 	var receiverTree = mapToTree(receivers);
-	var state = im.fromJS(init);
+	var state = immutableFromJS(init);
 	return function dispatch(path, modify = (a => a), defaultValue = null) {
 		state = state.updateIn(path, defaultValue, modify);
-		var found = receiverTree.getIn(path, new im.Map());
-		var receivers = im.Iterable.isIterable(found) ? found.valueSeq().flatten() : im.List.of(found);
-		var stream = new Bacon.Bus();
-		stream.plug(Bacon.fromBinder(sink => {
+		var found = receiverTree.getIn(path, new Map());
+		var receivers = Iterable.isIterable(found) ? found.valueSeq().flatten() : List.of(found);
+		var stream = new Bus();
+		stream.plug(baconFromBinder(sink => {
 			receivers.forEach(receiver => {
 				var result = receiver(state.toJS(), (...args) => stream.plug(dispatch(...args)));
 				if(result) sink(result);
