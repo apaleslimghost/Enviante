1caec4fb96f64fe1881ead6c42fd9a995e675328
Matt Brennan <mattyb1000@gmail.com>
2015-12-23 21:13:20 +0000

    formatting

--- src/index.js
+++ src/index.js
@@ -5,17 +5,21 @@ import mapToTree from '@quarterto/immutable-map-to-tree';
 export default function dispatcher(receivers, init = {}) {
 	var receiverTree = mapToTree(receivers);
 	var state = immutableFromJS(init);
+	
 	return function dispatch(path, modify = (a => a), defaultValue = null) {
-		state = state.updateIn(path, defaultValue, modify);
 		var found = receiverTree.getIn(path, new Map());
 		var receivers = Iterable.isIterable(found) ? found.valueSeq().flatten() : List.of(found);
 		var stream = new Bus();
+
+		state = state.updateIn(path, defaultValue, modify);
+
 		stream.plug(baconFromBinder(sink => {
 			receivers.forEach(receiver => {
 				var result = receiver(state.toJS(), (...args) => stream.plug(dispatch(...args)));
 				if(result) sink(result);
 			});
 		}));
+
 		return stream;
 	};
 };
