e36a6a9117b80d2519face47fd7da9d2788374a1
Matt Brennan <mattyb1000@gmail.com>
2015-12-23 19:36:48 +0000

    no need for explicit intent()

--- src/index.js
+++ src/index.js
@@ -1,22 +1,20 @@
 var im = require('immutable');
 var Bacon = require('baconjs');
 
-export var intent = (path, modify = (i => i), defaultValue = null) => ({path, modify, defaultValue});
-
 export var dispatcher = (receivers, init = {}) => {
 	var receiverMap = receivers.reduce(
 		(map, receiver) => map.updateIn(receiver.receives, current => current ? current.push(receiver) : im.List.of(receiver)),
 		new im.Map()
 	);
 	var state = im.fromJS(init);
-	var dispatch = intent => {
-		state = state.updateIn(intent.path, intent.defaultValue, intent.modify);
-		var found = receiverMap.getIn(intent.path, new im.Map());
+	var dispatch = (path, modify = (a => a), defaultValue = null) => {
+		state = state.updateIn(path, defaultValue, modify);
+		var found = receiverMap.getIn(path, new im.Map());
 		var receivers = im.Iterable.isIterable(found) ? found.valueSeq().flatten() : im.List.of(found);
 		var stream = new Bacon.Bus();
 		stream.plug(Bacon.fromBinder(sink => {
 			receivers.forEach(receiver => {
-				var result = receiver(state.toJS(), subIntent => stream.plug(dispatch(subIntent)));
+				var result = receiver(state.toJS(), (...args) => stream.plug(dispatch(...args)));
 				if(result) sink(result);
 			});
 		}));
